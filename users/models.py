9babe53792fe79fae87720d51599bd15a084cc4b
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-23 16:42:52 +0530

    added customuser model with uuid to take email instead of username

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+import uuid
+
+class CustomUser(AbstractUser):
+    '''This is a customuser model since we need uuid for the id, and it also takes email id instead of username'''
+    uuid = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False)
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+class Profile(models.Model):
+    '''This defines the user model, which has the username and his confirmed email'''
+    id = models.UUIDField( 
+         primary_key = True, 
+         default = uuid.uuid4, 
+         editable = False)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    email_confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user)
